ab137d517c46f3232962492fb039e2561ee358c1
M Mon <0xmarkhydra@gmail.com>
2025-03-31 12:08:50 +0700

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,12 @@
+// @ts-ignore
 export * from './upload';
+// @ts-ignore
 export * from './books';
+// @ts-ignore
 export * from './questions';
+// @ts-ignore
 export * from './exams';
+// @ts-ignore
 export * from './codes';
+// @ts-ignore
 export * from './dashboard';
